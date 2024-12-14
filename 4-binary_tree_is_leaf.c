07ee7fa12868791a5610a84963788e6cd3637e37
FadySabry712 <fadysabry95@yahoo.com>
2024-12-14 14:44:13 -0800

    task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,10 +10,7 @@
 
 int binary_tree_is_leaf(binary_tree_t *node)
 {
-	if (node != NULL)
-	{
-		if (node->left == NULL && node->right == NULL)
-			return (1);
-	}
+	if (node != NULL && node->left == NULL && node->right == NULL)
+		return (1);
 	return (0);
 }
