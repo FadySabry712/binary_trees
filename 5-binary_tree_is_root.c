8efa9bcd68a14025add4ec5701128858165b9642
FadySabry712 <fadysabry95@yahoo.com>
2024-12-14 11:58:02 -0800

    task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -11,8 +11,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node != NULL)
 	{
-		if (node->right == NULL && node->left == NULL)
-			return (0);
+		if (node->parent == NULL)
+			return (1);
 	}
-	return (1);
+	return (0);
 }
